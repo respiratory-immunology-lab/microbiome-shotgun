b0f616e1580030b4ede1272b4252b35f217a1c61
Celine Pattaroni <40220143+cpattaroni@users.noreply.github.com>
2021-06-25 14:48:51 +1000

    Update fastq_wrapper.sh

--- fastq_wrapper.sh
+++ fastq_wrapper.sh
@@ -18,15 +18,10 @@ for f in *.fastq.gz
   do
   Basename=${f%_L00*}
   ## merge R1
-  ls ${Basename}_L00*_R1_001.fastq.gz | xargs cat > ${Basename}_merged_R1.fastq.gz
+  ls ${Basename}_L00*_R1_001.fastq.gz | xargs cat > ${Basename}_R1.fastq.gz
   ## merge R2
-  ls ${Basename}_L00*_R2_001.fastq.gz | xargs cat > ${Basename}_merged_R2.fastq.gz
+  ls ${Basename}_L00*_R2_001.fastq.gz | xargs cat > ${Basename}_R2.fastq.gz
   done
-
-# Check whether the merging has worked
-#-bash-4.2$ zcat []_L001_R1_001.fastq.gz | echo $((`wc -l`/4))
-#-bash-4.2$ zcat []_L002_R1_001.fastq.gz | echo $((`wc -l`/4))
-#-bash-4.2$ zcat []_merged_R1.fastq.gz | echo $((`wc -l`/4))
 
 # Remove individual files to make space
 rm -rf *L00*
